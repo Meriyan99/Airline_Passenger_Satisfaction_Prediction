8b9a7b9369510ff48a42d7a4677e85900736160d
Meriyan <it21076916@my.sliit.lk>
2023-10-03 12:54:04 +0530

    Added Dataset

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     #this will gives 3 parameters, but the last one contains information regarding when error occured
